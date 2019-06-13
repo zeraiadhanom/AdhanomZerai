c52f02febc6e081a417b05e623cd172ba8ed9e6f
zeraiadhanom <zeraiadhanom@gmail.com>
2019-06-13 08:55:49 -0700

    'update'

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
+import image2 from './image2.jpg';
+import './Home.css';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h2> About</h2>
+      <div clasName="main">
+        <img
+          src={image2}
+          className="image2"
+          alt="image2"
+          width="150"
+          height="120"
+        />
+        <h2>Brief Introduction</h2>
         <p>
-          Cras facilisis urna ornare ex volutpat, et convallis erat elementum.
-          Ut aliquam, ipsum vitae gravida suscipit, metus dui bibendum est, eget
-          rhoncus nibh metus nec massa. Maecenas hendrerit laoreet augue nec
-          molestie. Cum sociis natoque penatibus et magnis dis parturient
-          montes, nascetur ridiculus mus.
+          My name is Richard; i am a software Test enginer. I have over seven
+          years’ experience of software integration, test development,
+          validation strategies. I executed manual and automation test on
+          Android and Windows operating system for various mobile chipset.
+        </p>
+        <p>
+          {' '}
+          I am pasionate web developer. I have created dynamic, secure Web site
+          from scratch; Please take a look some of my projects at portofilio.
         </p>
 
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+        <p>Please contact at below.</p>
       </div>
     );
   }
--- src/components/projects.js
+++ src/components/projects.js
@@ -4,18 +4,18 @@ class Projects extends Component {
   render() {
     return (
       <div className="projects">
-        <p2>Some projects accomplished to demonstrate skill level</p2>
-
-        <p>
-          Mauris sem velit, vehicula eget sodales vitae,rhoncus eget sapien:
-        </p>
-        <ol>
-          <li>Nulla pulvinar diam</li>
-          <li>Facilisis bibendum</li>
-          <li>Vestibulum vulputate</li>
-          <li>Eget erat</li>
-          <li>Id porttitor</li>
-        </ol>
+        <h2>Projects</h2>
+        <section className="border-top">
+          <div className="project_image">
+            <a
+              className="img-parent"
+              href="https://symphesis.herokuapp.com/"
+              target="_blank"
+            >
+              <img alt="Symphesis app image" src="./images/symphesis.png" />
+            </a>
+          </div>
+        </section>
       </div>
     );
   }
