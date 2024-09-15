61663e6a52fe27bd9ed34940477f84e6eb85b397
zeraiadhanom <zeraiadhanom@gmail.com>
2024-09-14 19:14:49 -0700

    Updates

---
+++ precache-manifest.85d33c4e32f1fa6530d094ab9bb2748f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "656c1344f75b846fa95f34b6d9f89b7d",
+    "url": "/AdhanomZerai/index.html"
+  },
+  {
+    "revision": "fc6fb524a83c0169a8a5",
+    "url": "/AdhanomZerai/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "22f3bf11eec754802ede",
+    "url": "/AdhanomZerai/static/css/main.db4f502a.chunk.css"
+  },
+  {
+    "revision": "fc6fb524a83c0169a8a5",
+    "url": "/AdhanomZerai/static/js/2.ae131f57.chunk.js"
+  },
+  {
+    "revision": "22f3bf11eec754802ede",
+    "url": "/AdhanomZerai/static/js/main.7de593cd.chunk.js"
+  },
+  {
+    "revision": "d495dd9fc39eb6a2e986",
+    "url": "/AdhanomZerai/static/js/runtime~main.ea9066fa.js"
+  },
+  {
+    "revision": "37354d52e14bcc9969313c7977a66fc7",
+    "url": "/AdhanomZerai/static/media/ActivTracker.37354d52.png"
+  },
+  {
+    "revision": "1365e05976ad328aeadf59f5e7f37055",
+    "url": "/AdhanomZerai/static/media/MovieAPI.1365e059.JPG"
+  },
+  {
+    "revision": "7de6afb317f12e21373d0fa4b4b102ad",
+    "url": "/AdhanomZerai/static/media/RadioStation.7de6afb3.jpg"
+  },
+  {
+    "revision": "dc1bbc4d4c2caada84052398d2501110",
+    "url": "/AdhanomZerai/static/media/Ychanel.dc1bbc4d.PNG"
+  },
+  {
+    "revision": "ae71f98b17a8e9c5d4630a9359519daf",
+    "url": "/AdhanomZerai/static/media/ZERAIADHANOM.ae71f98b.pdf"
+  },
+  {
+    "revision": "fc28501d8c32b8844e30a395f439c06c",
+    "url": "/AdhanomZerai/static/media/church.fc28501d.jpg"
+  },
+  {
+    "revision": "4779ac13e5e778a9a7edbdeb48b384bf",
+    "url": "/AdhanomZerai/static/media/peace.4779ac13.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portofolio/precache-manifest.b37554b63575e9e6d1ce419d731b8403.js"
+  "/AdhanomZerai/precache-manifest.85d33c4e32f1fa6530d094ab9bb2748f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-portofolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AdhanomZerai/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
